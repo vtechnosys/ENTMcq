e8ca657a08758b2e22238e7e47d9b91bbe49e7af
unknown <vertextechnosys@gmail.com>
2022-08-22 12:04:11 +0530

    22-8-22

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,70 @@
-import { useState,useEffect } from "react";
-
+import { useState,useEffect } from "react"
+import { useIdleTimer, workerTimers } from 'react-idle-timer'
 function Header()
 {
+
+
+   // Set timeout values
+   const timeout = 30 * 1000
+   const promptTimeout = 1000 * 30
+ 
+   // Modal open state
+   const [open, setOpen] = useState(false)
+ 
+   // Time before idle
+   const [remaining, setRemaining] = useState(0)
+ 
+   const onPrompt = () => {
+     // onPrompt will be called after the timeout value is reached
+     // In this case 30 minutes. Here you can open your prompt. 
+     // All events are disabled while the prompt is active. 
+     // If the user wishes to stay active, call the `reset()` method.
+     // You can get the remaining prompt time with the `getRemainingTime()` method,
+     setOpen(true)
+     setRemaining(promptTimeout)
+   }
+   
+   const onIdle = () => {
+     // onIdle will be called after the promptTimeout is reached.
+     // In this case 30 seconds. Here you can close your prompt and 
+     // perform what ever idle action you want such as log out your user.
+     // Events will be rebound as long as `stopOnMount` is not set.
+     setOpen(false)
+     setRemaining(0)
+     window.location.href='/';
+   }
+   
+   const onActive = () => {
+     // onActive will only be called if `reset()` is called while `isPrompted()` 
+     // is true. Here you will also want to close your modal and perform
+     // any active actions. 
+     setOpen(false)
+     setRemaining(0)
+   }
+ 
+   const { getRemainingTime, isPrompted, activate } = useIdleTimer({
+     timeout,
+     promptTimeout,
+     onPrompt,
+     onIdle,
+     onActive
+   })
+ 
+  //  const handleStillHere = () => {
+  //    setOpen(false)
+  //    activate()
+  //  }
+ 
+   useEffect(() => {
+     const interval = setInterval(() => {
+       if (isPrompted()) {
+         setRemaining(Math.ceil(getRemainingTime() / 1000))
+       }
+     }, 1000)
+     return () => {
+       clearInterval(interval)
+     }
+   }, [getRemainingTime, isPrompted])
 
   
   const [type, setType] = useState(() => {
@@ -18,6 +81,7 @@ function Header()
       window.location.href='/';
     }
   }
+ 
   useEffect(()=>{
     islog()
   },[])
@@ -401,6 +465,7 @@ function Header()
        </ul>)
           }
         </aside>
+       
     )
 }
 
